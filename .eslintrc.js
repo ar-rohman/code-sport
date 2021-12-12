688af732dfbb84ad35cbcb53de4b3c7bb11bbe4f
Rohman <rohmanpendo@gmail.com>
2021-12-12 17:52:51 +0700

    eslint & prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,17 @@ module.exports = {
     env: {
         node: true,
     },
-    extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
     parserOptions: {
-        parser: "babel-eslint",
+        parser: '@babel/eslint-parser',
+        // parser: 'babel-eslint',
     },
+    plugins: ['prettier'],
     rules: {
-        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-        indent: ['error', 4],
-        "quotes": ["error", "single", { "avoidEscape": true }]
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        // indent: ['error', 4],
+        // quotes: ['error', 'single', { avoidEscape: true }],
+        'prettier/prettier': 'error',
     },
 };
