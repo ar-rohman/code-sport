cfd63e955ba01ca1ab3fe4c5b95e81940272b8d3
Rohman <rohmanpendo@gmail.com>
2021-12-13 08:04:44 +0700

    add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     content: ['./src/**/*.{html,js,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
     plugins: [],
 };
