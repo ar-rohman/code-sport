ab4e42816505464884d7f3178ac45e77febc3710
Rohman <rohmanpendo@gmail.com>
2021-12-14 18:34:29 +0700

    Fix axios plugin config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import axios from './plugins/axios';
 import './assets/styles/index.css';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(axios).mount('#app');
+// const app = createApp(App);
+// app.use(store);
+// app.use(router);
+// app.use(axios);
+// app.mount('#app');
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,18 +1,14 @@
 import axios from 'axios';
 
-let installed = false;
-
 export default {
-    install(Vue) {
-        if (installed) return;
-        Vue.prototype.$axios = (param) => {
-            const instance = axios.create({
+    install: (app) => {
+        app.config.globalProperties.$axios = (param) => {
+            const config = {
                 url: param,
                 baseURL: process.env.VUE_APP_API_URL,
                 headers: { 'X-Auth-Token': process.env.VUE_APP_API_KEY },
-            });
-            return instance;
+            };
+            return axios(config);
         };
-        installed = true;
     },
 };
