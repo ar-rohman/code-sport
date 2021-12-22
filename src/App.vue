e5fc7fa651d1be09ee2f6c28b444c8f23a8db222
Rohman <rohmanpendo@gmail.com>
2021-12-22 17:05:35 +0700

    add back header at team detail page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="font-sans min-h-screen max-w-screen-xl mx-auto text-gray-700 bg-white">
-        <app-header />
+        <div v-if="showAppHeader">
+            <app-header />
+        </div>
+        <div v-else>
+            <back-header />
+        </div>
         <main class="m-4 sm:mx-10">
             <router-view />
         </main>
@@ -9,11 +14,17 @@
 
 <script>
 import AppHeader from '@/components/AppHeader.vue';
-
+import BackHeader from '@/components/BackHeader.vue';
 export default {
     name: 'App',
     components: {
         AppHeader,
+        BackHeader,
+    },
+    computed: {
+        showAppHeader() {
+            return this.$route.name !== 'TeamDetailPage';
+        },
     },
 };
 </script>
---
+++ src/components/BackHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex w-full p-3 border-b sticky top-0 bg-white">
+        <button @click.stop="$router.go(-1)">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor">
+                <path
+                    fill-rule="evenodd"
+                    d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+                    clip-rule="evenodd" />
+            </svg>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BackHeader',
+};
+</script>
