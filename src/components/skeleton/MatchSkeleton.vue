a28d6f02a1731a86cfa759b866dc6df7623bbdf0
Rohman <rohmanpendo@gmail.com>
2021-12-28 11:48:15 +0700

    feat: add match skeleton

---
+++ src/components/skeleton/MatchSkeleton.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="flex flex-col mb-10">
+        <div
+            class="mb-2 font-semibold text-gray-600 w-64 h-6 bg-gray-200 rounded animate-pulse"></div>
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-sky-50">
+                            <tr>
+                                <th
+                                    scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden sm:table-cell">
+                                    Date
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Home
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Score
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Away
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden sm:table-cell">
+                                    Group
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden lg:table-cell">
+                                    Status
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white">
+                            <template v-for="index in 50" :key="index">
+                                <tr class="sm:hidden animate-pulse">
+                                    <td class="px-6 pt-4" colspan="2">
+                                        <div class="h-4 w-24 rounded bg-gray-200"></div>
+                                    </td>
+                                    <td class="px-6 pt-4">
+                                        <div class="flex justify-end">
+                                            <div class="h-4 w-24 rounded bg-gray-200"></div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="border-b border-gray-200 animate-pulse">
+                                    <td class="px-6 pb-4 sm:py-4 hidden sm:table-cell">
+                                        <div class="h-5 w-48 rounded bg-gray-200"></div>
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <div class="flex justify-end">
+                                            <div class="h-5 w-32 rounded bg-gray-200"></div>
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <div class="flex justify-center">
+                                            <div class="h-5 w-12 rounded bg-gray-200"></div>
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <div class="h-5 w-32 rounded bg-gray-200"></div>
+                                    </td>
+                                    <td class="px-6 py-4 hidden sm:table-cell">
+                                        <div class="h-5 w-16 rounded bg-gray-200"></div>
+                                    </td>
+                                    <td class="px-6 py-4 hidden lg:table-cell">
+                                        <div class="h-5 w-24 rounded bg-gray-200"></div>
+                                    </td>
+                                </tr>
+                            </template>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MatchSkeleton',
+};
+</script>
--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -1,96 +1,101 @@
 <template>
     <h1 class="font-bold text-xl mb-8">Matches</h1>
-    <template v-for="(dataMatch, idx) in matches" :key="idx">
-        <div class="flex flex-col mb-10" v-if="dataMatch.data.length > 0">
-            <div class="mb-2 font-semibold text-gray-600" :id="dataMatch.stage">
-                {{ stringFormat(dataMatch.stage) }}
-            </div>
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <table class="min-w-full divide-y divide-gray-200">
-                            <thead class="bg-sky-50">
-                                <tr>
-                                    <th
-                                        scope="col"
-                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden sm:table-cell">
-                                        Date
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Home
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        {{ isShow(dataMatch.isScore) ? 'Score' : '' }}
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Away
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden sm:table-cell"
-                                        v-if="isShow(dataMatch.isGroup)">
-                                        Group
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden lg:table-cell">
-                                        Status
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody class="bg-white">
-                                <template v-for="(match, index) in dataMatch.data" :key="index">
-                                    <tr class="sm:hidden">
-                                        <td class="px-6 pt-4 text-xs text-gray-400" colspan="2">
-                                            {{ dateFormat(match.utcDate) }}
-                                        </td>
-                                        <td
-                                            class="px-6 pt-4 text-xs text-right text-gray-400"
+    <template v-if="isLoading">
+        <MatchSkeleton />
+    </template>
+    <template v-else>
+        <template v-for="(dataMatch, idx) in matches" :key="idx">
+            <div class="flex flex-col mb-10" v-if="dataMatch.data.length > 0">
+                <div class="mb-2 font-semibold text-gray-600" :id="dataMatch.stage">
+                    {{ stringFormat(dataMatch.stage) }}
+                </div>
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-sky-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden sm:table-cell">
+                                            Date
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Home
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            {{ isShow(dataMatch.isScore) ? 'Score' : '' }}
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Away
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden sm:table-cell"
                                             v-if="isShow(dataMatch.isGroup)">
-                                            {{ stringFormat(match.group) }}
-                                        </td>
+                                            Group
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden lg:table-cell">
+                                            Status
+                                        </th>
                                     </tr>
-                                    <tr class="border-b border-gray-200">
-                                        <td
-                                            class="px-6 pb-4 sm:py-4 text-sm text-gray-500 hidden sm:table-cell">
-                                            {{ dateFormat(match.utcDate) }}
-                                        </td>
-                                        <td class="px-6 py-4 text-right text-sm text-gray-500">
-                                            {{ match.homeTeam }}
-                                        </td>
-                                        <td class="px-6 py-4 text-center text-sm text-gray-500">
-                                            {{
-                                                isShow(dataMatch.isScore)
-                                                    ? `${match.score.homeTeam} : ${match.score.awayTeam}`
-                                                    : 'vs'
-                                            }}
-                                        </td>
-                                        <td class="px-6 py-4 text-sm text-gray-500">
-                                            {{ match.awayTeam }}
-                                        </td>
-                                        <td
-                                            class="px-6 py-4 text-sm text-gray-500 hidden sm:table-cell"
-                                            v-if="isShow(dataMatch.isGroup)">
-                                            {{ stringFormat(match.group) }}
-                                        </td>
-                                        <td
-                                            class="px-6 py-4 text-sm text-gray-500 hidden lg:table-cell">
-                                            {{ stringFormat(match.status) }}
-                                        </td>
-                                    </tr>
-                                </template>
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody class="bg-white">
+                                    <template v-for="(match, index) in dataMatch.data" :key="index">
+                                        <tr class="sm:hidden">
+                                            <td class="px-6 pt-4 text-xs text-gray-400" colspan="2">
+                                                {{ dateFormat(match.utcDate) }}
+                                            </td>
+                                            <td
+                                                class="px-6 pt-4 text-xs text-right text-gray-400"
+                                                v-if="isShow(dataMatch.isGroup)">
+                                                {{ stringFormat(match.group) }}
+                                            </td>
+                                        </tr>
+                                        <tr class="border-b border-gray-200">
+                                            <td
+                                                class="px-6 pb-4 sm:py-4 text-sm text-gray-500 hidden sm:table-cell">
+                                                {{ dateFormat(match.utcDate) }}
+                                            </td>
+                                            <td class="px-6 py-4 text-right text-sm text-gray-500">
+                                                {{ match.homeTeam }}
+                                            </td>
+                                            <td class="px-6 py-4 text-center text-sm text-gray-500">
+                                                {{
+                                                    isShow(dataMatch.isScore)
+                                                        ? `${match.score.homeTeam} : ${match.score.awayTeam}`
+                                                        : 'vs'
+                                                }}
+                                            </td>
+                                            <td class="px-6 py-4 text-sm text-gray-500">
+                                                {{ match.awayTeam }}
+                                            </td>
+                                            <td
+                                                class="px-6 py-4 text-sm text-gray-500 hidden sm:table-cell"
+                                                v-if="isShow(dataMatch.isGroup)">
+                                                {{ stringFormat(match.group) }}
+                                            </td>
+                                            <td
+                                                class="px-6 py-4 text-sm text-gray-500 hidden lg:table-cell">
+                                                {{ stringFormat(match.status) }}
+                                            </td>
+                                        </tr>
+                                    </template>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </template>
     </template>
     <back-to-top />
 </template>
@@ -98,16 +103,19 @@
 <script>
 import { mapGetters, mapActions } from 'vuex';
 import BackToTop from '@/components/BackToTop.vue';
+import MatchSkeleton from '@/components/skeleton/MatchSkeleton.vue';
 
 export default {
     name: 'MatchesPage',
     components: {
         BackToTop,
+        MatchSkeleton,
     },
     data() {
         return {
             matches: null,
             country: 'ID',
+            isLoading: true,
         };
     },
     created() {
@@ -210,6 +218,7 @@ export default {
                 currentStage = element.stage;
             });
             this.matches = dataMatches;
+            this.isLoading = false;
         },
         ...mapActions({
             setMatch: 'match/set',
