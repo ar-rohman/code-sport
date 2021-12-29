c0e9820a79943bd0b93073e702b6fd6d10847d54
Rohman <rohmanpendo@gmail.com>
2021-12-29 09:27:55 +0700

    chore: back to top when page open (scroll behavior)

--- src/router/index.js
+++ src/router/index.js
@@ -42,6 +42,9 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+        return savedPosition || { top: 0 };
+    },
 });
 
 const defaultTitle = 'Code Sport';
